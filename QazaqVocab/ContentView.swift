9d6e66494bdef3aa1789169768ebc357d18b0442
Beksultan <batonpro123@gmail.com>
2026-08-26 17:40:38 +0500

    Initial Commit

---
+++ QazaqVocab/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QazaqVocab
+//
+//  Created by Beksultan Mussin on 26.08.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QazaqVocab/QazaqVocabApp.swift
@@ -0,0 +1,17 @@
+//
+//  QazaqVocabApp.swift
+//  QazaqVocab
+//
+//  Created by Beksultan Mussin on 26.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct QazaqVocabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
